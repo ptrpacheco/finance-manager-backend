23edee05c2f0f00ed84fc909bda7c9defcf75604
Pietro Pacheco <pietro.pacheco19@gmail.com>
2026-01-11 18:16:01 -0300

    Model user

---
+++ src/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+});
+
+// hash da senha antes de salvar no banco de dados
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
