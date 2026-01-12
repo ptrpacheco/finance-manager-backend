799fa96b8233951e1d5f88e988a98a5d4574cc5d
Pietro Pacheco <pietro.pacheco19@gmail.com>
2026-01-11 21:51:12 -0300

    Adição timestamps

--- src/models/User.js
+++ src/models/User.js
@@ -1,34 +1,37 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
   },
-  surname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 // hash da senha antes de salvar no banco de dados
 userSchema.pre("save", async function (next) {
-  if(!this.isModified("password")) {
+  if (!this.isModified("password")) {
     return next();
   }
 
