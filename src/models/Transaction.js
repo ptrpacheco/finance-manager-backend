4cbad1b774e4a5ca22ecbf10adcbb75ff68dcba9
Pietro Pacheco <pietro.pacheco19@gmail.com>
2026-01-11 21:51:26 -0300

    Model transaction

---
+++ src/models/Transaction.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
